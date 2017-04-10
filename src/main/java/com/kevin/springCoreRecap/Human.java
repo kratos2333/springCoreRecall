9dcd9d455fc7e0878d72859393f503815a6ab444
kevin <kevinjia1984@gmail.com>
2017-04-10 16:46:00 +1000

    Spring Core practice project

---
+++ src/main/java/com/kevin/springCoreRecap/Human.java
@@ -0,0 +1,5 @@
+package com.kevin.springCoreRecap;
+
+public interface Human {
+	public void speak();
+}
---
+++ src/main/java/com/kevin/springCoreRecap/Teacher.java
@@ -0,0 +1,10 @@
+package com.kevin.springCoreRecap;
+
+public class Teacher implements Human {
+
+	@Override
+	public void speak() {
+		System.out.println("I am a teacher");
+	}
+
+}
